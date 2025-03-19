46d53218bf313b3c24973c25edcab94e8889fcd8
Max Vigueras <mvt.2000@hotmail.com>
2025-03-18 21:52:09 -0600

    Sockets: Socket comunication implemented.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//App de express
 const app = express()
-const path = require('path')
+
+//Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./src/sockets/socket');
+
 
 const publicPath = path.resolve( __dirname, 'public' );
-
 app.use(express.static(publicPath));
 
-app.listen( 3000, ( err ) => {
+server.listen( process.env.PORT, ( err ) => {
 	
 	if ( err ) throw new Error( err )
 
-		console.log('Servidor corriendeo en puerto', 3000);
-	})
+	console.log('Servidor corriendo en puerto', process.env.PORT);
+})
